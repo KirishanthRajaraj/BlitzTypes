754ca8148b0088d7d7dd2c59a2d0d2ae303ccbdb
KirishanthRajaraj <KirishanthRajaraj@NOTEBOOK-KIRI>
2024-08-27 17:59:04 +0200

    mocking up profile component 2

---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Profile = () => {
+  // properties to show
+  /*
+    username
+    joined at
+    amount of tests started
+    amount of seconds or hours or min typed
+    edit bio?
+    ----
+    change username as option, maybe with request?
+    ----
+    preferred language
+    preferred typing speed
+    highscore (in every category?)
+    date of highscore set?
+    
+  */ 
+  
+    return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Profile
