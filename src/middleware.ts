dba73d5c9e4539d8e3a68f67af8a03e1b789daee
KirishanthRajaraj <rajarajkirishanth@gmail.com>
2024-09-16 19:06:47 +0200

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getUser } from './client/User';
-import { getTokenFetch } from './client/Authentication';
 
 const protectedRoutes = ['/profile']
 const authRoutes = ['/login', '/register']
